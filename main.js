7ddf7df93fa3cf3f19c16f69076ad40b4619b6fa
Wilson Chen <wilson.chen@codeimmersives.com>
2020-02-13 18:27:34 -0500

    finish

--- main.js
+++ main.js
@@ -20,60 +20,125 @@
 // Even though we'll USUALLY be calling this function with a number indicating the challenge we're on, THIS TIME call it with the parameter 'Banner Function Works!'. You should see the below result:
 //
 // ###### Challenge Banner Function Works! #####
-
+function bannerMaker($){
+    console.log(`###### Challenge ${$} #####`)
+}
+bannerMaker('banner function works')
 
 // 2.
 //
 // Console out the numbers from 1-20, but only if they're divisible by 3.
 
 // Use `if` and a modulus operation.
-
-
+let num = 1
+let n = 20
+while (num <= n){
+    if (num % 3 === 0)
+    console.log(num)
+    num++
+}
+bannerMaker('3')
 // 3.
 //
 // Same problem as #2, but no `if`s, `mods`, or `but`s (and no, `but` is not a JavaScript tool). There may be more ways to solve this problem without `if` or `%` (there usually are in JavaScript!), but if you did it the way we did, you should run the loop 6 times with console log per loop iteration.
+num = 3
+n = 20
+while (num <= n){
+    console.log(num)
+    num = num +3
+}
 
-
-
+bannerMaker(4)
 // 4.
 //
 // Console out the numbers from 1-20. BACKWARDS.
-
-
+num = 1
+n = 20
+while (num <= n){
+    console.log(n)
+    n--
+}
+bannerMaker(5)
 // 5.
 //
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
-
-
+num = 0
+let str = 'Boy howdy am I good at this!'
+while (num < str.length){
+    console.log(str[num])
+    num++
+}
+bannerMaker(6)
 // 6.
 //
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
-
-
+num = 1
+str = 'And getting better every day.'
+while (num < str.length){
+    console.log(str[num])
+    num = num + 3
+}
+bannerMaker(7)
 // 7. 
 //
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
+str = 'Am I the best?'
+num = 0
+// let upCaseStr = str.toUpperCase
+for (let i = 0; i < str.length ; i++ ){
+    console.log(str[i].toUpperCase())
 
-
+}
+// while (num < str.length){
+    
+//     console.log(upCaseStr[num])
+//     num = num + 1
+// }
+bannerMaker(8)
 // 8.
 //
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
+str = 'Whoa, I am the best!'
+num = 0
+let space = 0
 
+while (num < str.length){
+    if (str[num] === ' '){ 
+    space = space + 1
+    console.log(str[num])
+}  
+    else if(space === 2){
+    console.log(str[num].toUpperCase())
+}
+    else if(space !== 2){
+        console.log(str[num])
+}
+    num = num + 1
+       // if (str[num] === 'a' && str[num+1] === 'm'|| str[num] === 'm')
+    
+    // if (str[num] === 'm')
+    // {console.log(str[num].toUpperCase())
 
+//  } 
+}
+bannerMaker(9)
 // 9.
 //
 // For the string 'I am become death, destroyer of worlds.', console out the letters
 //  individually, but backwards. That is, the first console log should be
 // `.`, followed by `s`, followed by `d`, and so on.
-
-
+str = 'I am become death, destroyer of worlds.'
+for (let i = str.length-1; i >= 0;i--){
+    console.log(str[i])
+}
+bannerMaker(10)
 // 10.
 //
-const names = 'Colin|Mesuara|Drew|In Seop|Emma|Ben|J|Ryan|Olgy|Mike|Chris|John';
+const names = 'Colin|Mesuara|Denis|Wilson|Michael|Patrick|DeAundre|Jumary|Marc|Bogdan|Sharod|David|LaToddra|Carlos';
 
 // Console out the individual letters of each person in class today, which are
 // in the string above. Every time you start a new name (including the first
@@ -83,6 +148,16 @@ const names = 'Colin|Mesuara|Drew|In Seop|Emma|Ben|J|Ryan|Olgy|Mike|Chris|John';
 
 // Don't print the pipes!
 
+
+for (let i = 0; i < names.length; i ++){
+    // console.log('Here today is:')
+    if (names[i]==='|'){
+        console.log('Here today is:')
+    }else 
+    console.log(names[i])
+    
+    
+}
 
 // 11.
 //
